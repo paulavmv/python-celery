010bea9db06e13c98cab863b1cb4f36ebcdb48b9
root <root@DESKTOP-GHQPR21.localdomain>
2021-04-22 18:57:14 -0300

    feat: initial celery configuration

---
+++ app/src/worker.py
@@ -0,0 +1,11 @@
+
+from celery import Celery
+import os
+
+#get the message broker url
+redis_url = os.environ.get("REDIS_URL", 'redis://localhost:6379')
+
+celery = Celery(__name__, backend="%s/0" % redis_url, broker="%s/1" % redis_url)
+
+def create_app(app):
+    celery.conf.update(app.config)
