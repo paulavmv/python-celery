066187f0c0cd1f3d2dd46c0934c7c74e452d9968
root <root@DESKTOP-GHQPR21.localdomain>
2021-04-25 09:23:45 -0300

    feat: registering task

--- app/src/worker.py
+++ app/src/worker.py
@@ -1,11 +1,12 @@
-
 from celery import Celery
 import os
 
 #get the message broker url
 redis_url = os.environ.get("REDIS_URL", 'redis://localhost:6379')
 
-celery = Celery(__name__, backend="%s/0" % redis_url, broker="%s/1" % redis_url)
+celery = Celery(__name__, backend="%s/0" % redis_url, broker="%s/1" % redis_url,
+                include=('app.src.business.hard_work',))
+
 
 def create_app(app):
     celery.conf.update(app.config)
