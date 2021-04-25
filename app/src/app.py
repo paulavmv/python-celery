34548e135a845c3d39c44857a65748ef99b86b81
root <root@DESKTOP-GHQPR21.localdomain>
2021-04-25 09:24:20 -0300

    feat: adding service

--- app/src/app.py
+++ app/src/app.py
@@ -1,12 +1,16 @@
-from flask import Flask
+from flask import Flask, request
 from .worker import create_app
+from .business.hard_work import send_email
 
 app = Flask(__name__)
 
 create_app(app)
 
-@app.route('/')
-def hello_world():
-    return 'Hello World'
+@app.route('/send_email', methods=['POST'])
+def replace():
+
+    result = send_email.delay()
+
+    return {'message': 'Email sent.'}
 
 
